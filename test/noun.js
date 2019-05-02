dcdba10b8005a1b8211badf608a662770c8e7ca9
David Kerschner <dkerschner@hcoop.net>
2019-05-02 14:38:43 -0700

    killed useless junk in tests/noun.js

--- test/noun.js
+++ test/noun.js
@@ -36,51 +36,6 @@ var genCell = gen.nested(mkCellGen, genAtom),
     genNoun = gen.oneOf([genAtom, genCell]);
 
 module.exports = {
-  genAtom: genAtom,
-  genCell: genCell,
-  genNoun: genNoun,
-};
-
-/*
-const { check, gen } = require('tape-check')
-
-test('maps work like maps', check(
-
-test('addition is commutative', check(gen.int, gen.int, (t, numA, numB) => {
-    t.plan(1)
-    t.equal(numA + numB, numB + numA)
-}));
-*/
-
-/*
-function randomAtom() {
-  var c, i, bytes = Math.floor(Math.random() * 4) + 1;
-  var c = new BigInteger();
-  var d = new BigInteger();
-  c.fromInt(0);
-  for ( i = 0; i < bytes; ++i ) {
-    d.fromInt(Math.floor(Math.random() * 0xff));
-    c = c.shiftLeft(8);
-    c = c.xor(d);
-  }
-  return new noun.Atom.Atom(c);
-}
-
-function randomCell(depth) {
-  return new noun.Cell(randomNoun(depth+1), randomNoun(depth+1));
-}
-
-function randomNoun(depth) {
-  if ( depth > 10 || Math.random() > 0.5) {
-    return randomAtom();
-  }
-  else {
-    return randomCell(depth);
-  }
-}
-*/
-
-module.exports = {
   equals: equals,
   genAtom: genAtom,
   genCell: genCell,
